8889eb214d2c3390537d70c76f2a683045383b36
A Ritesh Reddy <riteshaletisvn@gmail.com>
2025-11-05 11:59:19 +0530

    database started

--- AION_hostings/main.py
+++ AION_hostings/main.py
@@ -13,21 +13,30 @@ models={
 async def generate_actionset(message: dict):
 	if check_if_loaded("actionset") is None:
 		load_model("actionset",model["actionset"])
-	ollama.chat()
+	ollama.generate(
+			model=models["actionset"],
+			prompt=message
+		)
 	return
 
 @app.post("/generate_reason")
 async def generate_reason(message: dict):
 	if check_if_loaded("actionset") is None:
 		load_model("actionset",model["actionset"])
-	ollama.chat()
+	ollama.chat(
+			model=models["reason"],
+			messages=message
+		)
 	return
 
 @app.post("/generate_logic")
 async def generate_logic(message: dict):
 	if check_if_loaded("logic") is None:
 		load_model("logic",model["logic"])
-	ollama.chat()
+	ollama.generate(
+			model=models["logic"],
+			prompt=message
+	)
 	return
  
 if __name__=="__main__":
